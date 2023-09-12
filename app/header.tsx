9015880d0afd09610486830b6aec4256d5b54756
metagravity <metagravity0@gmail.com>
2023-09-12 22:58:23 +0100

    dark mode added succes

--- app/header.tsx
+++ app/header.tsx
@@ -1,12 +1,10 @@
+"use client"
+
+import Themebutton from "@/components/Themebutton";
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
-import { Sun, Moon } from "lucide-react";
+
 
 export default function header() {
-    function setTheme(arg0: string): void {
-        throw new Error("Function not implemented.");
-    }
-
     return (
         <main>
             <div className="flex font-bold items-center justify-center p-3 text-center bg-grey-100 fw-50">
@@ -25,28 +23,7 @@ export default function header() {
                         <a href="/chat">Chat</a>
                     </Button>
                 </div>
-            </div>
-            <div>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" size="icon">
-                            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                            <span className="sr-only">Toggle theme</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem onClick={() => setTheme("light")}>
-                            Light
-                        </DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => setTheme("dark")}>
-                            Dark
-                        </DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => setTheme("system")}>
-                            System
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <Themebutton />
             </div>
         </main>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -9,30 +9,14 @@ export default function Home() {
   return (
     <main>
       <div>
-        <h1 className='text-center font-bold fw-40 p-2'>This is flame</h1>
+        <p>h</p>
+        {/* <h1 className='text-center font-bold fw-40 p-2'>This is flame</h1>
         <div className='p-6 max-w-sm mx-auto bg-white shadow rounded-xl items-center text-center group block hover:bg-red-300 hover:ring-green-200'>
           <div className='text-xl font-medium text-black'> 
             Flame 🔥
           </div>
           <p className='text-slate-500 group hover: text-sm font-semibold'>⚡Just a bunch of features I guess</p>
-        </div>
-        <div className='m-6 p-3 max-w-sm mx-auto bg-white shadow rounded-xl text-center '>
-          <div className='text-xl font-medium text-black'>
-            Juice 🧃
-          </div>
-          <p className='text-slate-500 text-sm font-semibold'>💡With enough juice and idea you can create anything </p>
-        </div>
-        <div className='p-3 max-w-sm mx-auto bg-white shadow rounded-xl text-center'>
-         <div className='text-xl font-medium text-black'>
-          Clean 🧼
-        </div>
-        <p className='text-slate-500 text-sm font-semibold'>🧽 Keep your codebase clean and organized</p>
-      </div>
-        <div className='item-center text-center p-4'>
-          <Button>
-            <Mail className='mr-2 h-4 w-4 text-center' /><a href="/login">Login with email</a>
-          </Button>
-        </div>
+        </div> */}
       </div>
     </main>
   )
