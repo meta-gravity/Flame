dc28e1baaabe162887f23ee1cbac19db7f495259
metagravity <metagravity0@gmail.com>
2023-09-14 15:42:53 +0100

    middleware done .ts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,4 @@
-import { redirect } from 'next/navigation'
-import { authOptions } from "../api/auth/[...nextauth]/route"
-import { getServerSession } from "next-auth"
-
 export default async function Dashboard() {
-    const session = await getServerSession(authOptions)
-
-    if (!session) {
-        redirect('/api/auth/signin')
-    }
-
     return (
         <main>
             <h1 className="items-center text-center h-screen">Hello world I am secret</h1>
