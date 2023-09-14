27abac7a84e6d7cabb7ae3ab2402eecbce2b5c75
metagravity <metagravity0@gmail.com>
2023-09-15 00:58:49 +0100

    cleaned the way the files are

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Dashboard() {
-  return (
-    <main>
-      <h1 className="items-center text-center h-screen">
-        Hello world I am secret
-      </h1>
-    </main>
-  );
-}
